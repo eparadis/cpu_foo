0f5c1814f72ef39a2072aa97b5a5d5fe48f0e212
Ed Paradis <eparadis@users.noreply.github.com>
2021-10-22 21:39:00 -0700

    capture compilation process

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 
-
-
+# this is how repl.it was compiling main.c
+#clang-7 -pthread -lm -o main main.c
+clang -lm -o main main.c
 
 # compile ex1.asm and run its output, formatting the simulators output to characters
 bash ./super_crapsm.sh ex1.asm out && ./main out | grep OUT | awk '{ printf "%s", $2 } END { printf "\n" }'
