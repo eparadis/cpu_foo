4c7a171887d30ed70b55e024002c77aa26d75d9c
eparadis <eparadis@users.noreply.github.com>
2021-11-21 06:48:27 +0000

    add echo-until-z test to suite

--- tests/setup.sh
+++ tests/setup.sh
@@ -12,9 +12,14 @@ echo "assembling tiny_echo"
 bash ../super_crapsm.sh tiny_echo.asm tiny_echo.bin
 echo "assembling capital_alphabet"
 bash ../super_crapsm.sh capital_alphabet.asm capital_alphabet.bin
+echo "assembling echo_until_Z"
+bash ../super_crapsm.sh echo_until_Z.asm echo_until_Z.bin
 
 # send a small program to display a single character into the loader
 ./send.py tiny_echo.bin | ./main loader.bin
 
 # print the alphabet
 ./main capital_alphabet.bin
+
+# send some chars and then stop at Z
+echo "zzzWXYZ123" | ./main echo_until_Z.bin
