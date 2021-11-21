c2d72744d57bb27e51b992f9ce5833ceb021e6f7
eparadis <eparadis@users.noreply.github.com>
2021-11-21 06:36:54 +0000

    add alphabet test to test suite

---
+++ tests/capital_alphabet.asm
@@ -0,0 +1,11 @@
+# print capital A thru Z
+LOAD 00 0E
+ADDI 01
+STORE FF FF
+CMP 5A
+JLT 00 03 
+DB AA
+DW 40 34
+# comment
+
+# ^^ blank line
--- tests/setup.sh
+++ tests/setup.sh
@@ -5,15 +5,16 @@ gcc -lm -o main main.c
 popd
 cp ../main .
 
-# compile ex3.asm (a binary loader)
+# assemble a binary loader. it takes a single byte length followed by data to place at 0100, then jumps there
 echo "assembling loader"
 bash ../super_crapsm.sh loader.asm loader.bin
-echo "assembling test"
+echo "assembling tiny_echo"
 bash ../super_crapsm.sh tiny_echo.asm tiny_echo.bin
+echo "assembling capital_alphabet"
+bash ../super_crapsm.sh capital_alphabet.asm capital_alphabet.bin
 
 # send a small program to display a single character into the loader
-# first byte is length, followed by the data
-#echo "loading 7 byte program: LOAD 00 05  STORE FF FF  DB AA\n"
-#echo -ne \\x07\\x08\\x00\\x05\\x09\\xff\\xff\\xaa | ./main loader
-#echo
 ./send.py tiny_echo.bin | ./main loader.bin
+
+# print the alphabet
+./main capital_alphabet.bin
