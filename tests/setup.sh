591ce5498e0b7b2af1daee86cff5d18e94e5799e
Ed Paradis <eparadis@users.noreply.github.com>
2021-11-23 03:05:53 -0800

    replace super_crap with crap

--- tests/setup.sh
+++ tests/setup.sh
@@ -7,15 +7,15 @@ cp ../main .
 
 # assemble a binary loader. it takes a single byte length followed by data to place at 0100, then jumps there
 echo "assembling loader"
-bash ../super_crapsm.sh loader.asm loader.bin
+bash ./crapsm.sh loader.asm loader.bin
 echo "assembling tiny_echo"
-bash ../super_crapsm.sh tiny_echo.asm tiny_echo.bin
+bash ./crapsm.sh tiny_echo.asm tiny_echo.bin
 echo "assembling capital_alphabet"
-bash ../super_crapsm.sh capital_alphabet.asm capital_alphabet.bin
+bash ./crapsm.sh capital_alphabet.asm capital_alphabet.bin
 echo "assembling echo_until_Z"
-bash ../super_crapsm.sh echo_until_Z.asm echo_until_Z.bin
+bash ./crapsm.sh echo_until_Z.asm echo_until_Z.bin
 echo "assembling reverse_line"
-bash ../super_crapsm.sh reverse_line.asm reverse_line.bin
+bash ./crapsm.sh reverse_line.asm reverse_line.bin
 
 # send a small program to display a single character into the loader
 ./send.py tiny_echo.bin | ./main loader.bin
