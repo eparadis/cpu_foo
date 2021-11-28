fbe35598206241b77f1a91a1101c80ebd61ba8fe
Ed Paradis <eparadis@users.noreply.github.com>
2021-11-27 17:09:36 -0800

    use what labels we can, which is not many

--- tests/reverse_line.asm
+++ tests/reverse_line.asm
@@ -1,16 +1,18 @@
 # push a null as a marker
 PUSH
 # echo until new line
+:top
 LOAD FF FF
 STORE FF FF
 PUSH
 CMP 0A
 JE 00 10
-JMP 00 01
+JMP top
 # pop off stack until a null
+:next
 POP
 CMP 00
 JE 00 1C
 STORE FF FF
-JMP 00 10
+JMP next
 HALT
