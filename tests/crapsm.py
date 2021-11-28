e3b24cc1a89d88105228e834d38be6d513562e9a
Ed Paradis <eparadis@users.noreply.github.com>
2021-11-27 16:58:11 -0800

    add simple labels to crapsm
    The assembler is still only one-pass, so this only
    allows labels that are defined before their use

--- tests/crapsm.py
+++ tests/crapsm.py
@@ -10,22 +10,31 @@ if len(sys.argv) != 3:
 infilename = sys.argv[1]
 outfilename = sys.argv[2]
 
-def convert( word):
+def convert( word, labels):
   if word == 'DB' or word == 'DW' or word == '':
     return (0, '')
+  if word in labels:
+    return (2, "%0.2X %0.2X" % (labels[word] >> 8, labels[word] & 0xFF ) )
   mnuemonics = [ 'LOAD', 'STORE', 'ADDI', 'NOP', 'CMP', 'JE', 'JLT', 'JMP', 'PUSH', 'POP', 'HALT', 'SHIFTL']
   opcodes =    [ '08'  , '09'   , '0A'  , '00' , '0B' , '0C', '0D' , '0E' , '0F'  , '10' , '11'  , '12' ]
   if word in mnuemonics:
     return (1, opcodes[mnuemonics.index(word)])
   return (1, word)
 
+labels = {}
+
 with open(infilename, 'r') as infile, open(outfilename, 'wb') as outfile:
   position = 0
   for line in infile:
     if line[0] == '#':
       continue
-    for word in line.split(' '):
-      (length, val) = convert(word.strip())
+    if line[0] == ':':
+      label = line.split()[0][1:]
+      print(label, end=' ')
+      labels[label] = position
+      continue
+    for word in line.split():
+      (length, val) = convert(word.strip(), labels)
       if length == 0:
         continue
       print(val, end=' ')
--- tests/shift_fun.asm
+++ tests/shift_fun.asm
@@ -1,7 +1,13 @@
 # shift_fun.asm
 # test out the SHIFTL instruction
+# jump over our labels
+JMP 00 04
+# stored constant 
+:letter
+DB 7E
+
 # print a stored value
-LOAD 00 25
+LOAD letter
 STORE FF FF
 # shift, subtract 127, and print it a few times
 SHIFTL
@@ -20,10 +26,6 @@ SHIFTL
 ADDI 80
 STORE FF FF
 HALT
-
-# stored value
-DB 7E
-
 
 # bash script
 # for i in `seq 0 7`; do  printf '%d %X\n' ${i} $(( ( ( 0xFE << ${i} ) + 0x80 ) & 0xFF  )) ; done
